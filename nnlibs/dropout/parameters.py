5ee2e5982a90a6ded53589274338b74a34b73156
Synthaze <florian.malard@gmail.com>
2021-08-23 23:49:43 +0200

    dropout

--- nnlibs/dropout/parameters.py
+++ nnlibs/dropout/parameters.py
@@ -1,21 +1,29 @@
 # EpyNN/nnlibs/dropout/paremeters.py
+# Related third party imports
+import numpy as np
 
 
 def dropout_compute_shapes(layer, A):
-    """Compute forward shapes and dimensions for layer.
+    """Compute forward shapes and dimensions from input for layer.
     """
     X = A    # Input of current layer
 
-    layer.fs['X'] = layer.fs['D'] = X.shape  # (m .. n)
+    layer.fs['X'] = X.shape    # (m, .. )
 
-    layer.d['m'] = layer.fs['X'][0]        # Number of samples (m)
-    layer.d['n'] = X.size // layer.d['m']  # Number of 1D features (n)
+    layer.d['m'] = layer.fs['X'][0]          # Number of samples  (m)
+
+    # Shape for dropout mask
+    layer.fs['D'] = [1 if i in layer.d['a'] else layer.fs['X'][i] for i in range(X.ndim)]
+
+    # Scaling factor for signal
+    layer.d['s'] = np.zeros(layer.fs['D']).size / X.size
+    layer.d['s'] *= (1 - layer.d['d'])
 
     return None
 
 
 def dropout_initialize_parameters(layer):
-    """Initialize parameters for layer.
+    """Initialize trainable parameters from shapes for layer.
     """
     # No parameters to initialize for Dropout layer
 
@@ -31,7 +39,7 @@ def dropout_compute_gradients(layer):
 
 
 def dropout_update_parameters(layer):
-    """Update parameters for layer.
+    """Update parameters from gradients for layer.
     """
     # No parameters to update for Dropout layer
 
